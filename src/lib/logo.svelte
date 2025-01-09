e66800f3b1fa0834fced6788eaedfafa5f7355a0
Andrew McNutt <mcnutt.andrew@gmail.com>
2025-01-09 16:19:16 -0700

    Big simplify (#1)

--- src/lib/logo.svelte
+++ src/lib/logo.svelte
@@ -1,4 +1,49 @@
 <script lang="ts">
+	const ratio = 64 / 84;
+	const height = 200;
+	const width = height * ratio;
 </script>
 
-<div class="h-[3em] border-2 border-black">todo</div>
+<div class="ml-4">
+	<svg {width} {height} viewBox="0 0 64 84" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect x="16.7977" y="1.47662" width="5.57" height="81.8303" fill="#F5F5DC" />
+		<rect x="24.6111" y="1.47662" width="5.57" height="81.8303" fill="#F5F5DC" />
+		<rect x="32.4243" y="1.47662" width="5.57" height="81.8303" fill="#F5F5DC" />
+		<rect x="0.671265" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="8.4845" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="16.2977" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="24.1111" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="31.9243" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="39.7378" y="17.9166" width="6.57" height="65.8904" fill="#BE0000" />
+		<rect x="47.5509" y="17.9166" width="6.57" height="65.8904" fill="#BE0000" />
+		<rect x="55.3643" y="17.9166" width="6.57" height="65.8904" fill="#BE0000" />
+		<rect x="0.671265" y="72.7378" width="7.5672" height="11.0692" fill="white" />
+		<rect x="8.4845" y="66.6146" width="7.5672" height="17.1923" fill="white" />
+		<rect x="16.2977" y="61.1613" width="7.5672" height="22.6458" fill="white" />
+		<rect x="24.1111" y="58.9607" width="7.5672" height="24.8462" fill="white" />
+		<rect x="31.9243" y="58.9607" width="7.5672" height="24.8462" fill="white" />
+		<rect x="39.7378" y="61.1613" width="7.5672" height="22.6457" fill="white" />
+		<rect x="47.5509" y="66.6146" width="7.5672" height="17.1923" fill="white" />
+		<rect x="55.3643" y="72.7378" width="7.5672" height="11.0691" fill="white" />
+		<rect x="16.7977" y="1.47662" width="5.57" height="81.8303" fill="#F5F5DC" />
+		<rect x="24.6111" y="1.47662" width="5.57" height="81.8303" fill="#F5F5DC" />
+		<rect x="32.4243" y="1.47662" width="5.57" height="81.8303" fill="#F5F5DC" />
+		<rect x="0.671265" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="8.4845" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="16.2977" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="24.1111" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="31.9243" y="0.976624" width="6.57" height="82.8303" fill="#BE0000" />
+		<rect x="39.7378" y="17.9166" width="6.57" height="65.8904" fill="#BE0000" />
+		<rect x="47.5509" y="17.9166" width="6.57" height="65.8904" fill="#BE0000" />
+		<rect x="55.3643" y="17.9166" width="6.57" height="65.8904" fill="#BE0000" />
+		<rect x="0.671265" y="72.7378" width="7.5672" height="11.0692" fill="white" />
+		<rect x="8.4845" y="66.6146" width="7.5672" height="17.1923" fill="white" />
+		<rect x="16.2977" y="61.1613" width="7.5672" height="22.6458" fill="white" />
+		<rect x="24.1111" y="58.9607" width="7.5672" height="24.8462" fill="white" />
+		<rect x="31.9243" y="58.9607" width="7.5672" height="24.8462" fill="white" />
+		<rect x="39.7378" y="61.1613" width="7.5672" height="22.6457" fill="white" />
+		<rect x="47.5509" y="66.6146" width="7.5672" height="17.1923" fill="white" />
+		<rect x="55.3643" y="72.7378" width="7.5672" height="11.0691" fill="white" />
+		<path d="M0.364502 81.3273H63.2601" stroke="#BE0000" stroke-width="3" />
+	</svg>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,18 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import Logo from '$lib/logo.svelte';
 	import { page } from '$app/stores';
-	import { base } from '$app/paths';
 
 	const sections = [
-		{
-			name: 'home',
-			href: `${base}/`
-		},
-
-		{
-			name: 'people',
-			href: `${base}/people`
-		},
-		{
-			name: 'papers',
-			href: `${base}/papers`
-		}
-	];
+		// {
+		// 	name: 'home',
+		// 	href: `${base}/`
+		// },
+		// {
+		// 	name: 'events',
+		// 	href: `${base}/events`
+		// }
+	] as any[];
 
 	$: pageName =
 		$page.url.pathname.split('/').filter((x) => x && x !== 'uwdata.github.io')[0] || 'home';
@@ -27,7 +21,7 @@
 
 <div class="w-full flex justify-center">
 	<div class="w-full md:max-w-4xl m-0 font-sans px-4 py-3">
-		<nav class="md:hidden flex justify-between text-xs uppercase">
+		<nav class="md:hidden flex justify-around text-xs uppercase">
 			{#each sections as section}
 				{#if pageName === section.name}
 					<a class="font-semibold" href={section.href}>{section.name}</a>
@@ -39,18 +33,15 @@
 
 		<div class="flex flex-col w-full md:pl-24">
 			<header class="flex flex-row justify-between mb-3">
-				<div class="text-2xl flex md:text-8xl mt-1">
+				<div class="text-2xl flex md:text-8xl mt-1 justify-around">
 					<h1 class="font-medium text-utah">
 						<a href="/">
 							<div>utah vis</div>
 							<div>collective</div>
 						</a>
 					</h1>
-					<div class="mb-0.5"><Logo /></div>
 				</div>
-				<div class="md:hidden w-9">
-					<!-- <img src={`${base}/images/logo/idl-300.png`} alt="IDL logo" /> -->
-				</div>
+				<div class="mb-0.5 hidden md:block"><Logo /></div>
 			</header>
 			<div class="hidden md:flex justify-between static top-0 w-full">
 				<nav class="flex mb-4 w-full justify-between">
